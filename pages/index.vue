1505e4ffb2737ea5c52181bec314eecd13d06e34
bmoree <bmoree@propeller-im.nl>
2022-02-17 22:03:22 +0100

    scaffold basic content homepage

--- pages/index.vue
+++ pages/index.vue
@@ -16,5 +16,44 @@
         </div>
       </div>
     </section>
+    <section class="container">
+      <div
+        class="
+          -mt-48
+          z-10
+          py-16
+          relative
+          sm:grid sm:grid-cols-2
+          lg:grid-cols-4
+          grid-flow-row
+          gap-8
+        "
+      >
+        <div
+          v-for="n in 4"
+          :key="n"
+          class="rounded-lg p-6 shadow-lg bg-white mt-4"
+        >
+          <h2>Some other title</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis
+            corporis debitis iusto velit assumenda sint quia accusantium magnam
+            quae fugit delectus, soluta iste earum illum! Nihil unde architecto
+            quasi dolor libero voluptatem velit, temporibus ad consequuntur
+            soluta aut fugiat possimus.
+          </p>
+        </div>
+      </div>
+    </section>
+    <section class="py-12 container bg-gray-200">
+      <h2>Some other title</h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis corporis
+        debitis iusto velit assumenda sint quia accusantium magnam quae fugit
+        delectus, soluta iste earum illum! Nihil unde architecto quasi dolor
+        libero voluptatem velit, temporibus ad consequuntur soluta aut fugiat
+        possimus.
+      </p>
+    </section>
   </main>
 </template>
